e1851bd5a4389850c4b6069dc56d9a3a0748d844
João Gabriel <jgdiniz20@gmail.com>
2026-04-24 14:15:20 -0300

    Adição da tabela services

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, ForeignKey, Numeric
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
@@ -34,3 +34,15 @@ class Customer(User):
     __mapper_args__ = {
         "polymorphic_identity": "CLIENTE",
     }
+
+class Service(Base):
+    __tablename__ = "services"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    prestador_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    duration: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Service(name='{self.name}', duration={self.duration}, price={self.price})>"
